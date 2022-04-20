d8af697a39f4a52d02ae915ff57868171d313d51
mjmon <mjmon1995@gmail.com>
2022-04-20 16:17:22 +0800

    refactor pagination

--- gorobin/src/index.js
+++ gorobin/src/index.js
@@ -16,79 +16,108 @@ firebase_admin_1.default.initializeApp({
     databaseURL: process.env.DATABASEURL
 });
 const db = firebase_admin_1.default.firestore();
-const axiosInstance = axios_1.default.create();
-let url = "https://toysrus.gorobinsons.ph/collections/all?_=pf&tag=TRU%20RP%20GALLERIA&page=1";
+const now = firebase_admin_1.default.firestore.Timestamp.now();
+//TRU RP GALLERIA
 let parsePattern = "html body main div#shopify-section-static-collection div.productgrid--wrapper ul.boost-pfs-filter-products.productgrid--items.products-per-row-4 li div.productitem";
 const AxiosInstance = axios_1.default.create();
-AxiosInstance.get(url)
-    .then(// Once we have data returned ...
-// Once we have data returned ...
-response => {
-    const html = response.data; // Get the HTML from the HTTP request
-    const $ = cheerio_1.default.load(html); // Load the HTML string into cheerio
-    const allElements = $(parsePattern);
-    const allProducts = [];
-    allElements.each((index, element) => {
-        const name = $(element).find('.productitem--title > a').text().replace("/n", "").trim();
-        const imageSrc = $(element).find('a.productitem--image-link > figure').find('img').attr('src');
-        const price1Data = $(element).find('.price--compare-at > span.money').text();
-        const price2Data = $(element).find('.price--main > span.money').text();
-        const price1 = normalizeStringPrice(price1Data);
-        const price2 = normalizeStringPrice(price2Data);
-        let origPrice = 0;
-        let promoPrice = 0;
-        if (price1 === "") {
-            // if there's no discounted price display, price2 is the original price
-            // and promo is zero
-            origPrice = parseFloat(price2);
-            promoPrice = 0;
-        }
-        else {
-            origPrice = parseFloat(price1);
-            promoPrice = parseFloat(price2);
-        }
-        allProducts.push({
-            name,
-            origPrice: origPrice,
-            promoPrice: promoPrice,
-            image: `https:${imageSrc}`,
+let tag = "TRU RP GALLERIA";
+//up to page 10 for now
+let pages = [...Array(10).keys()];
+Promise.all(pages.map((page) => {
+    let url = `https://toysrus.gorobinsons.ph/collections/all?_=pf&tag=${tag}&page=${page}`;
+    performScrapeAndWrite(url);
+}));
+function performScrapeAndWrite(url) {
+    AxiosInstance.get(encodeURI(url))
+        .then(// Once we have data returned ...
+    // Once we have data returned ...
+    response => {
+        const html = response.data; // Get the HTML from the HTTP request
+        const $ = cheerio_1.default.load(html); // Load the HTML string into cheerio
+        const allElements = $(parsePattern);
+        const allProducts = [];
+        allElements.each((index, element) => {
+            const name = $(element).find('.productitem--title > a').text().replace("/n", "").trim();
+            const imageSrc = $(element).find('a.productitem--image-link > figure').find('img').attr('src');
+            const price1Data = $(element).find('.price--compare-at > span.money').text();
+            const price2Data = $(element).find('.price--main > span.money').text();
+            const price1 = normalizeStringPrice(price1Data);
+            const price2 = normalizeStringPrice(price2Data);
+            let origPrice = 0;
+            let promoPrice = 0;
+            if (price1 === "") {
+                // if there's no discounted price display, price2 is the original price
+                // and promo is zero
+                origPrice = parseFloat(price2);
+                promoPrice = 0;
+            }
+            else {
+                origPrice = parseFloat(price1);
+                promoPrice = parseFloat(price2);
+            }
+            if (origPrice > 0 && promoPrice > 0) {
+                allProducts.push({
+                    name,
+                    origPrice: origPrice,
+                    promoPrice: promoPrice,
+                    image: `https:${imageSrc}`,
+                });
+            }
         });
-    });
-    console.log(allProducts);
-    const robinsonTenant = "k0DgV3qo5Rn9IFHMofME";
-    const jjshopTenant = "U9U1BbdULKZHJGI4iFUG";
-    const tenant = jjshopTenant;
-    const robinsonsOffersCollection = db.collection(`/Tenant/${tenant}/SpecialOffers`);
-    const doc = robinsonsOffersCollection.doc();
-    const prod5 = allProducts.at(5);
-    // fs.firestore.FieldValue.serverTimestamp()
-    // const now = Timestamp.now().toDate;
-    const now = firebase_admin_1.default.firestore.Timestamp.now();
-    // Timestamp.fromDate(new Date());
-    doc.set({
-        docId: doc.id,
-        isSpecialTenant: true,
-        title: prod5 === null || prod5 === void 0 ? void 0 : prod5.name,
-        description: '',
-        connectAttempt: [],
+        writeToFirestore(allProducts);
+    })
+        .catch(console.error);
+}
+function writeToFirestore(allProducts) {
+    const robinsonTenantId = "k0DgV3qo5Rn9IFHMofME";
+    const toysRusTenantId = "E1lYfBE3TcpAgO1x2cwr";
+    const tenantid = toysRusTenantId;
+    const toysRusCollection = db.collection(`/Tenant/${tenantid}/SpecialOffers`);
+    let dummyTenantInfo = {
+        address: "BGC Taguig",
+        allowBackorders: true,
+        businessName: "Toys R Us",
+        categoryCount: 0,
         connections: [],
         createDate: now,
-        startDate: now,
-        endDate: now,
-        updateDate: now,
-        fbId: null,
-        imageurl: prod5 === null || prod5 === void 0 ? void 0 : prod5.image,
-        images: [prod5 === null || prod5 === void 0 ? void 0 : prod5.image],
-        originalImages: [prod5 === null || prod5 === void 0 ? void 0 : prod5.image],
-        originalImageUrl: prod5 === null || prod5 === void 0 ? void 0 : prod5.image,
-        owner: 'JJ Shop',
-        origPrice: 500,
-        promoPrice: 400,
-        tenant: tenant,
-        tenantInfo: null,
+        docId: tenantid,
+        expireDate: now,
+        facebook: null,
+        isSetupComplete: true,
+        isSpecialTenant: true,
+        latitude: null,
+        longitude: null,
+        natureOfBusiness: "Others",
+        plan: "free",
+        topCardLayer: 0,
+        undeliveredReminder: "1 hour"
+    };
+    allProducts.forEach((prod) => {
+        const doc = toysRusCollection.doc();
+        doc.set({
+            docId: doc.id,
+            isSpecialTenant: true,
+            title: prod.name,
+            description: '',
+            connectAttempt: [],
+            connections: [],
+            createDate: now,
+            startDate: now,
+            endDate: now,
+            updateDate: now,
+            fbId: null,
+            imageUrl: prod === null || prod === void 0 ? void 0 : prod.image,
+            images: [prod === null || prod === void 0 ? void 0 : prod.image],
+            originalImages: [prod === null || prod === void 0 ? void 0 : prod.image],
+            originalImageUrl: prod === null || prod === void 0 ? void 0 : prod.image,
+            owner: '',
+            origPrice: prod === null || prod === void 0 ? void 0 : prod.origPrice,
+            promoPrice: prod === null || prod === void 0 ? void 0 : prod.promoPrice,
+            tenant: tenantid,
+            tenantInfo: dummyTenantInfo
+        });
     });
-})
-    .catch(console.error); //
+}
 /// remove peso sign, comma and extract the price only
 function normalizeStringPrice(price) {
     return price.replace("₱", "").replace(",", "").trim().split("\n")[0];
--- gorobin/src/index.ts
+++ gorobin/src/index.ts
@@ -16,28 +16,36 @@ fs.initializeApp({
 });
 
 const db = fs.firestore();
+      const now = fs.firestore.Timestamp.now();
 
-const axiosInstance = axios.create();
+//TRU RP GALLERIA
 
-let url = "https://toysrus.gorobinsons.ph/collections/all?_=pf&tag=TRU%20RP%20GALLERIA&page=1";
 let parsePattern = "html body main div#shopify-section-static-collection div.productgrid--wrapper ul.boost-pfs-filter-products.productgrid--items.products-per-row-4 li div.productitem";
 const AxiosInstance = axios.create();
 
-AxiosInstance.get(url)
+let tag = "TRU RP GALLERIA";
+
+//up to page 10 for now
+let pages = [...Array(10).keys()];
+
+Promise.all(pages.map((page) => {
+  let url = `https://toysrus.gorobinsons.ph/collections/all?_=pf&tag=${tag}&page=${page}`;
+  performScrapeAndWrite(url);
+}));
+
+
+function performScrapeAndWrite (url: string) { 
+AxiosInstance.get(encodeURI(url))
   .then( // Once we have data returned ...
     response => {
       const html = response.data; // Get the HTML from the HTTP request
       const $ = cheerio.load(html); // Load the HTML string into cheerio
           const allElements: cheerio.Cheerio = $(parsePattern); 
-
-          const allProducts: Product[] = [];
-          
+          const allProducts: Product[] = [];          
           
           allElements.each((index, element) => {
               const name: string = $(element).find('.productitem--title > a').text().replace("/n", "").trim();
-
               const imageSrc = $(element).find('a.productitem--image-link > figure').find('img').attr('src');
-
               const price1Data: string = $(element).find('.price--compare-at > span.money').text();
               const price2Data: string = $(element).find('.price--main > span.money').text();
 
@@ -56,64 +64,81 @@ AxiosInstance.get(url)
                   origPrice = parseFloat(price1);
                   promoPrice = parseFloat(price2);
               }
-              
+            if (origPrice > 0 && promoPrice > 0) { 
               allProducts.push({
                   name,
                   origPrice: origPrice,
                   promoPrice: promoPrice,
                   image: `https:${imageSrc}`,
               })
+            }
           });
-
-          console.log(allProducts);
       
-      const robinsonTenant = "k0DgV3qo5Rn9IFHMofME";
-      const jjshopTenant = "U9U1BbdULKZHJGI4iFUG";
-      const tenant = jjshopTenant;
-        
-          const robinsonsOffersCollection = db.collection(`/Tenant/${tenant}/SpecialOffers`);
-      const doc = robinsonsOffersCollection.doc();
-      
-      const prod5 = allProducts.at(5);
-      
-      // fs.firestore.FieldValue.serverTimestamp()
-      // const now = Timestamp.now().toDate;
-      const now = fs.firestore.Timestamp.now();
-        // Timestamp.fromDate(new Date());
-          
-      doc.set({
-            docId: doc.id,
-            isSpecialTenant: true,
-            title: prod5?.name,
-            description: '',
-            connectAttempt: [],
-            connections: [],
-            createDate: now,
-            startDate: now,
-            endDate: now,
-            updateDate: now,
-            fbId: null,
-            imageurl: prod5?.image,
-            images: [prod5?.image],
-            originalImages: [prod5?.image],
-            originalImageUrl: prod5?.image,        
-            owner: 'JJ Shop',
-            origPrice: 500,
-            promoPrice: 400,
-            tenant: tenant,
-            tenantInfo: null,
-          }
-        )
-          
-
-
-
+      writeToFirestore(allProducts);
+  
     }
   )
-  .catch(console.error); //
+  .catch(console.error); 
+}
+
+
+function writeToFirestore(allProducts: Product[]) { 
+      const robinsonTenantId = "k0DgV3qo5Rn9IFHMofME";
+      const toysRusTenantId = "E1lYfBE3TcpAgO1x2cwr";
+      const tenantid = toysRusTenantId;
+        
+      const toysRusCollection = db.collection(`/Tenant/${tenantid}/SpecialOffers`);
+
+      let dummyTenantInfo = {
+        address: "BGC Taguig",
+        allowBackorders: true,
+        businessName: "Toys R Us",
+        categoryCount: 0,
+         connections: [],
+        createDate: now,
+        docId: tenantid,
+        expireDate: now,
+        facebook: null,
+        isSetupComplete: true,
+        isSpecialTenant: true,
+        latitude: null,
+        longitude: null,
+        natureOfBusiness: "Others",
+        plan: "free",
+        topCardLayer: 0,
+        undeliveredReminder: "1 hour"
+      }
+
+      allProducts.forEach((prod) => { 
+        const doc = toysRusCollection.doc();
+        doc.set({
+              docId: doc.id,
+              isSpecialTenant: true,
+              title: prod.name,
+              description: '',
+              connectAttempt: [],
+              connections: [],
+              createDate: now,
+              startDate: now,
+              endDate: now,
+              updateDate: now,
+              fbId: null,
+              imageUrl: prod?.image,
+              images: [prod?.image],
+              originalImages: [prod?.image],
+              originalImageUrl: prod?.image,        
+              owner: '',
+              origPrice: prod?.origPrice,
+              promoPrice: prod?.promoPrice ,
+              tenant: tenantid,
+              tenantInfo: dummyTenantInfo
+        });
+      })
+}
 
 
 /// remove peso sign, comma and extract the price only
 function normalizeStringPrice(price: string): string {
     return price.replace("₱", "").replace(",", "").trim().split("\n")[0];
 }
+
